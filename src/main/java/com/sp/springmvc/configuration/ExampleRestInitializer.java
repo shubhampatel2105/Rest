ddc45dd7db6a7916fb859e7d9d690f248085fa22
Shubham Patel <`shubhampatel2105@gmail.com`>
2018-10-29 00:16:18 -0700

    intial commit

---
+++ src/main/java/com/sp/springmvc/configuration/ExampleRestInitializer.java
@@ -0,0 +1,22 @@
+package com.sp.springmvc.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ExampleRestInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[]{ExampleRestConfiguration.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[]{"/"};
+	}
+
+}
